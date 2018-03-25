12727e6cee986c9674e6d2bf82e40df35dd413d7
Dipta Dip <30615936+dipta1010@users.noreply.github.com>
2018-03-25 20:45:02 +0600

    Update BubbleSort.cpp

--- Algorithms/Sorting/BubbleSort.cpp
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -6,33 +6,32 @@ using namespace std;
 int arr[size], n;
 
 void BubbleSort(int arr[], int n) {
-	int i, j;
-	for (i=0; i<n; ++i) {
-		for (j=0; j<n-i-1; ++j) {
-			if (arr[j] > arr[j+1]) {
-                int tem = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = tem;
-			}
-		}
+    int i, j;
+    for (i=0; i<n; ++i) {
+        for (j=0; j<n-i-1; ++j) {
+            if (arr[j] > arr[j+1]) {
+                    int tem = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tem;
+	    }
 	}
+    }
 }
 
 void getInput() {
     printf("Enter the number of elements: ");
-	scanf("%d", &n);
+    scanf("%d", &n);
 
-	printf("Enter %d numbers:\n", n);
-	for(int i=0; i<n; i++) {
-		scanf("%d", &arr[i]);
-	}
+    printf("Enter %d numbers:\n", n);
+    for(int i=0; i<n; i++)
+        scanf("%d", &arr[i]);
 }
 
 void showOutput() {
     printf("\nAfter Sorting:\n", n);
-	for(int i=0; i<n; i++) {
-		printf("%d  ", arr[i]);
-	}
+    for(int i=0; i<n; i++) {
+        printf("%d  ", arr[i]);
+    }
 }
 
 int main()
