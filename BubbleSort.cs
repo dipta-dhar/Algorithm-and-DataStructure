55c9fca04b7562162f3cfb3c91a88db2073c30d7
Dipta Dip <30615936+dipta1010@users.noreply.github.com>
2018-03-25 20:58:21 +0600

    Add files via upload

---
+++ BubbleSort.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+ 
+public class Bubble_Sort
+{
+    public static void Main(string[] args)
+    {
+        int[] a = { 3, 0, 2, 5, -1, 4, 1 };
+        int t;
+        
+		Console.WriteLine("Before Sorting:");
+        foreach (int aa in a)
+            Console.Write(aa + " ");
+		
+        for (int p = 0; p <= a.Length - 2; p++) {
+            for (int i = 0; i <= a.Length - 2; i++) {
+                if (a[i] > a[i + 1]) {
+                    t = a[i + 1];
+                    a[i + 1] = a[i];
+                    a[i] = t;
+                }
+            }
+        }
+        Console.WriteLine("\n"+"After Sorting:");
+        foreach (int aa in a)
+            Console.Write(aa + " ");
+        Console.Write("\n");
+    }
+}
